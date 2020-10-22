f18f69c907506b3d4688018da7b74fd2552f350d
ysuzuki19 <42496938+ysuzuki19@users.noreply.github.com>
2020-10-23 00:00:04 +0900

    commit

--- dev/main.cpp
+++ dev/main.cpp
@@ -8,39 +8,44 @@
 
 using namespace std;
 
-#include <filesystem_compatible_os.hpp>
+//#include <filesystem_compatible_os.hpp>
+#include <filesystem>
+namespace fs = std::filesystem;
 #include <debug.hpp>
 #include <neuralnet.hpp>
 
 using namespace std;
-fs::path DatasetPath ("../dataset");
+std::filesystem::path DatasetDir ("../dataset");
 
-int main(){
+int main () {
 	//vector<size_t> layers = {512, 256, 128, 64, 10};
 	//vector<size_t> layers = {128, 64, 10};
 	vector<size_t> layers = {16, 4};
-	NeuralNet nn(layers);
-	debug::view("layer size: ", nn.getLayers().size());
-	debug::view("layers : ", nn.getLayers());
-	debug::view("neurons: ", nn.getNeurons());
-	debug::view("connect: ", nn.getConnect());
+	NeuralNet nn (layers, false);
 
-	fs::path train_path, test_path;
-	train_path = DatasetPath / "train";
-	test_path = DatasetPath / "test";
+	debug::view ("layer size: ", nn.getLayers ().size ());
+	debug::view ("neuron size: ", nn.getNeurons ().size ());
+	debug::view ("connect size: ", nn.getConnect ().size ());
+	//debug::view ("layers : ", nn.getLayers ());
+	//debug::view ("neurons: ", nn.getNeurons ());
+	//debug::view ("connect: ", nn.getConnect ());
 
-	//nn.load(train_path, test_path);
-	nn.load(train_path, test_path, true);
+	std::filesystem::path train_path, test_path;
+	train_path = DatasetDir / "train";
+	test_path = DatasetDir / "test";
+
+	nn.load (train_path, test_path);
+	//nn.load (train_path, test_path, true);
 	cout << "start fit " << endl;
-	nn.fit();
-	//nn.calcAcc();
-	//debug::view("accuracy: ", nn.getAcc());
-	debug::view("accuracy: ", nn.acc());
-	//cout << nn.acc() << endl;
-	//auto at = nn.getTrain(0);
-	//nn.predict();
-//	nn.view_train_data(0);
-//	nn.view_connect();
-//	nn.view_neurons();
+	nn.fit ();
+	//nn.calcAcc ();
+	//debug::view ("accuracy: ", nn.getAcc ());
+	//debug::view ("accuracy: ", nn.acc ());
+	//cout << nn.acc () << endl;
+	//auto at = nn.getTrain (0);
+	//nn.predict ();
+	//nn.view_train_data (0);
+	//nn.view_connect ();
+	//nn.view_neurons ();
 	return 0;
 }
